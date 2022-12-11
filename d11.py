0c7e1e5c7c4090e0d312fc8f69a414806895edfd
Pawel <wydrapawel@icloud.com>
2022-12-11 08:51:49 +0100

    Initial commit

---
+++ d11.py
@@ -0,0 +1,17 @@
+envelope_small = (10, 15)
+envelope_medium = (20, 30)
+envelope_big = (30, 30)
+
+prepare_gifts = [("Parfum", 12, 30),
+                 ("Socks", 10, 10),
+                 ("Book", 30, 25),
+                 ("Clock", 15, 15),
+                 ("Wallet", 5, 10)]
+
+for gift in prepare_gifts:
+    if gift[1] <= envelope_small[0] and gift[2] <= envelope_small[1]:
+        print(f"for {gift[0]} best choice is small envelope")
+    elif gift[1] <= envelope_medium[0] and gift[2] <= envelope_medium[1]:
+        print(f"for {gift[0]} best choice is medium envelope")
+    else:
+        print(f"for {gift[0]} best choice is big envelope")
